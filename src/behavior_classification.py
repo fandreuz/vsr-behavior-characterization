61427995570452feddf1c1317b39595f4ca5ab50
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-30 01:45:55 +0100

    incorporate indexes on avg_touch_area

--- src/behavior_classification.py
+++ src/behavior_classification.py
@@ -1,9 +1,11 @@
 from utils import beautiful_padded
+from indexes import *
 
 import pandas as pd
 import numpy as np
 import sys
 import csv
+from itertools import product
 
 from sklearn.cluster import KMeans
 
@@ -29,6 +31,24 @@ if len(sys.argv) > 1:
 else:
     clusters = 3
 
+# we define some mappings (i.e. weights) for the predictor 'avg.touch.area'
+avg_touch_area_mappings = []
+coeffs_grid = [0.1, 0.2, 0.5, 0.75, 1, 1.5]
+for mi, p, nfoot, nuniquefoot in product(
+    coeffs_grid, coeffs_grid, coeffs_grid, coeffs_grid
+):
+
+    def mppng(X, mi=mi, p=p, nfoot=nfoot, nuniquefoot=nuniquefoot):
+        return (
+            mi * k_mode_interval(X)
+            + p * k_purity(X)
+            + nfoot * k_nfootprints(X)
+            + nuniquefoot * k_unique_foorprints(X)
+        )
+
+    avg_touch_area_mappings.append(mppng)
+
+
 # we consider these two set of columns the key predictors
 columns = [
     "best→fitness→as[Outcome]→center.spectrum.x→[{}]".format(i)
@@ -46,40 +66,57 @@ training = list(range(10))
 training_data = [
     pd.read_csv("dataset/best.{}.txt".format(i), sep=";") for i in training
 ]
-
-X = np.concatenate(
-    [
-        training_data[i][training_data[i]["iterations"] == 204][columns]
-        for i in range(len(training))
-    ]
+concatenated_training_data = pd.concat(
+    data[data["iterations"] == 204] for data in training_data
 )
 
-kmeans = KMeans(n_clusters=clusters, algorithm="full").fit(X)
+# predictors used for unsupervised learning
+X = concatenated_training_data[columns]
 
-# these are our clusters
-labels = kmeans.labels_
+ata_key = "best→fitness→as[Outcome]→gait→avg.touch.area"
 
-dc = {}
+for avg_touch_mapping in avg_touch_area_mappings:
+    print('avg touh mapping', avg_touch_mapping(concatenated_training_data).max())
+    weighted_avg_touch = concatenated_training_data[
+        ata_key
+    ] * avg_touch_mapping(concatenated_training_data)
+    if not ata_key in X.columns:
+        X.insert(0, ata_key, weighted_avg_touch, allow_duplicates=True)
+    else:
+        X = X.assign(**{ata_key: weighted_avg_touch})
 
-l = X.shape[0] // len(training)
-for i in range(len(training)):
-    for row, label in zip(
-        training_data[i][training_data[i]["iterations"] == 204].itertuples(),
-        labels[l * i : l * (i + 1)],
-    ):
-        key = (row.terrain, row.shape)
-        if key not in dc:
-            dc[key] = []
-        dc[key].append(str(label))
+    Xv = X.melt().value
+    # print(np.max(X[ata_key]), np.min(X[ata_key]), np.max(Xv), np.min(Xv))
 
-print(beautiful_padded("SEED", " ".join(map(str, range(10)))))
-print("-" * 40 + "-" * 2 * len(training_data))
-for k, l in dc.items():
-    print(beautiful_padded(str(k) + " ->    ", " ".join(l)))
+    # unsupervised learning engine
+    kmeans = KMeans(n_clusters=clusters, algorithm="full").fit(X)
 
-with open("dataset/clusters.csv", "w") as f:
-    writer = csv.writer(f)
-    for t, s in dc.keys():
-        lb = dc[(t, s)]
-        row = [t, s, *lb]
-        writer.writerow(row)
+    # these are our clusters
+    labels = kmeans.labels_
+
+    dc = {}
+
+    l = X.shape[0] // len(training)
+    for i in range(len(training)):
+        for row, label in zip(
+            training_data[i][
+                training_data[i]["iterations"] == 204
+            ].itertuples(),
+            labels[l * i : l * (i + 1)],
+        ):
+            key = (row.terrain, row.shape)
+            if key not in dc:
+                dc[key] = []
+            dc[key].append(str(label))
+
+    print(beautiful_padded("SEED", " ".join(map(str, range(10)))))
+    print("-" * 40 + "-" * 2 * len(training_data))
+    for k, l in dc.items():
+        print(beautiful_padded(str(k) + " ->    ", " ".join(l)))
+
+    with open("dataset/clusters.csv", "w") as f:
+        writer = csv.writer(f)
+        for t, s in dc.keys():
+            lb = dc[(t, s)]
+            row = [t, s, *lb]
+            writer.writerow(row)
