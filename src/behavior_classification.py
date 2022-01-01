26110164f041a8292ec9f4511d1cb6312a143c14
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-01-01 10:49:25 +0100

    code cleanup

--- src/behavior_classification.py
+++ src/behavior_classification.py
@@ -35,6 +35,39 @@ from sklearn.preprocessing import StandardScaler
 # gait->num.unique.footprints' : Number of unique footprints in the n-gram.
 # gait->footprints'    : Comma-separated list of footprints in the main n-gram.
 
+
+# run an unsupervised learning classification using the given X. return the
+# clusters found in the experiment
+def run_experiment(X, training_data):
+    # unsupervised learning engine
+    kmeans = KMeans(n_clusters=n_clusters, algorithm="full").fit(
+        (X * weights)[:, random_columns]
+    )
+
+    # these are our clusters
+    labels = kmeans.labels_
+
+    clusters = tuple(Cluster(name=str(i)) for i in range(n_clusters))
+
+    l = X.shape[0] // len(training_data)
+    # we iterate over the files (i.e. the seed)
+    for i in range(len(training_data)):
+        # for each row we want to extract terrain/shape, and the cluster that
+        # robot was assigned to
+        for row, label in zip(
+            training_data[i][
+                training_data[i]["iterations"] == 204
+            ].itertuples(),
+            labels[l * i : l * (i + 1)],
+        ):
+            # we add the VSR to the cluster which it was assigned to
+            clusters[label].add(
+                VSR(shape=row.shape, training_terrain=row.terrain, seed=i)
+            )
+
+    return clusters
+
+
 if len(sys.argv) > 1:
     n_clusters = int(sys.argv[1])
 else:
@@ -185,32 +218,8 @@ for avg_touch_mapping, avg_touch_label in zip(
             random_columns = np.arange(0, X.shape[1])
         print("Using the following columns: " + str(random_columns))
 
-        # unsupervised learning engine
-        kmeans = KMeans(n_clusters=n_clusters, algorithm="full").fit(
-            (X * weights)[:, random_columns]
-        )
-
-        # these are our clusters
-        labels = kmeans.labels_
-
-        clusters = tuple(Cluster(name=str(i)) for i in range(n_clusters))
+        clusters = run_experiment(X, training_data)
         experiment_clusters.append(clusters)
-
-        l = X.shape[0] // len(training)
-        # we iterate over the files (i.e. the seed)
-        for i in range(len(training)):
-            # for each row we want to extract terrain/shape, and the cluster that
-            # robot was assigned to
-            for row, label in zip(
-                training_data[i][
-                    training_data[i]["iterations"] == 204
-                ].itertuples(),
-                labels[l * i : l * (i + 1)],
-            ):
-                # we add the VSR to the cluster which it was assigned to
-                clusters[label].add(
-                    VSR(shape=row.shape, training_terrain=row.terrain, seed=i)
-                )
 
         err, err_details, mapping = clusters_comparison(
             clusters, supervised_clusters
